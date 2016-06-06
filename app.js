d2f71882de3a7171c18b09a22e750042483ba5a5
U-LA\518374 <518374@DTV-F30686.LA.FRD.DIRECTV.com>
2016-06-06 15:43:33 -0700

    added fauna spritesheet generation

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ var server = require('http').Server(app);
 var walk    = require('walk');
 var images  = require('images');
 var terrain_images = {};
+var fauna_images = {};
 
 // Walker options
 var terrain_walker  = walk.walk('./resources/terrain', { followLinks: false });
+
+var fauna_walker = walk.walk('./resources/fauna', { followLinks: false });
 
 
 function getMaxWidth(sub_object) {
@@ -36,7 +39,6 @@ function createSpriteRow(sheet, values, row_idx) {
 
 terrain_walker.on('file', function(root, stat, next) {
     // Add this file to the list of files
-    // files.push(root + '/' + stat.name);
     var realname = root + '/' + stat.name;
     var basename = path.basename(root);
     var keywords = basename.split("_");
@@ -49,7 +51,7 @@ terrain_walker.on('file', function(root, stat, next) {
 
     if (terrain_images[main_key] !== undefined){
         var main = terrain_images[main_key];
-        if (main[sub_key] != undefined)
+        if (main[sub_key] !== undefined)
             main[sub_key].push(realname);
         else {
             main[sub_key] = [realname];
@@ -77,7 +79,63 @@ terrain_walker.on('end', function() {
 });
 
 
+fauna_walker.on('file', function(root, stat, next) {
+    var realname = root + '/' + stat.name;
+    var basename = path.basename(root);
+    var keywords = stat.name.split("_");
 
+    var entity_name = keywords[1];
+
+    var animation_name = keywords[2];
+    for (var i = 3; i < keywords.length - 1; i++) {
+        animation_name += "_" + keywords[i];
+    }
+
+    var animation_index = parseInt(keywords[keywords.length - 1].substring(0, 2), 10);
+    if (fauna_images[entity_name] !== undefined) {
+        var entity = fauna_images[entity_name];
+        if (entity[animation_name] !== undefined) {
+            entity[animation_name][animation_index] = realname;
+        } else {
+            entity[animation_name] = [];
+            entity[animation_name][animation_index] = realname;
+        }
+
+    } else {
+        fauna_images[entity_name] = {};
+        fauna_images[entity_name][animation_name] = [];
+        fauna_images[entity_name][animation_name][animation_index] = realname;
+    }
+
+    next();
+
+});
+
+fauna_walker.on('end', function() {
+    console.log(fauna_images);
+
+    Object.keys(fauna_images).forEach(function(entity_name) {
+        var filename = 'resources/generated/' + entity_name + '.png';
+        var entity = fauna_images[entity_name];
+        var animations = Object.keys(entity);
+
+        var widest = 0;
+        animations.forEach(function(animation_name, index) {
+            var width = entity[animation_name].length;
+            if (width > widest) {
+                widest = width;
+            }
+        });
+
+        var spritesheet = images(widest * 16, animations.length * 16);
+
+        animations.forEach(function(animation_name, index) {
+            createSpriteRow(spritesheet, entity[animation_name], index);
+        });
+
+        spritesheet.save(filename, {'quality': 90});
+    });
+});
 
 server.listen(8000);
 
